78776be7f5f0813d942eb8d392e7697f0df7f868
Liza-Veis <liza.veis14@gmail.com>
2024-01-03 20:54:52 +0100

    feat: prepare template

---
+++ src/components/app/app.tsx
@@ -0,0 +1,16 @@
+/**
+<main class="page">
+  <h1 class="page__title">Product List</h1>
+  <ul class="product-list">
+    <li class="product-list__item">Apple</li>
+    <li class="product-list__item">Carrot</li>
+    <li class="product-list__item">Papaya</li>
+  </ul>
+</main>
+**/
+
+const App = (): JSX.Element => {
+  return <h1>Hello World!</h1>;
+};
+
+export { App };
