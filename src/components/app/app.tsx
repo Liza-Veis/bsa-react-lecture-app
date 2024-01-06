f990852d6a31b16c319d8fabe13b623e868ec5d8
Liza-Veis <liza.veis14@gmail.com>
2024-01-06 19:41:37 +0100

    feat: add styles and icon for application

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,16 +1,5 @@
-/**
-<main class="page">
-  <h1 class="page__title">Product List</h1>
-  <ul class="product-list">
-    <li class="product-list__item">Apple</li>
-    <li class="product-list__item">Carrot</li>
-    <li class="product-list__item">Papaya</li>
-  </ul>
-</main>
-**/
-
 const App = (): JSX.Element => {
-  return <h1>Hello World!</h1>;
+  return <h1>Todo List</h1>;
 };
 
 export { App };
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import { App } from './components/app/app';
 
-import './styles.css';
+import './assets/css/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
