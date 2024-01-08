a6367f6fa8359da1a37cd13cf84064d862052c3d
Liza-Veis <liza.veis14@gmail.com>
2024-01-08 16:38:28 +0100

    feat: make items rendered dynamically

--- src/components/task-list/task-list.tsx
+++ src/components/task-list/task-list.tsx
@@ -1,25 +1,37 @@
+const items = [
+  {
+    id: 1,
+    title: 'Finish with the project',
+  },
+  {
+    id: 2,
+    title: 'Buy groceries',
+  },
+  {
+    id: 3,
+    title: 'Prepare for the exam',
+  },
+];
+
 const TaskList = (): JSX.Element => {
+  const hasItems = items.length !== 0;
+
   return (
-    <ul className="task-list">
-      <li className="task-item">
-        <label className="task-item__label">
-          <input type="checkbox" className="task-item__input" />
-          Finish with the project
-        </label>
-      </li>
-      <li className="task-item">
-        <label className="task-item__label">
-          <input type="checkbox" className="task-item__input" />
-          Buy Groceries
-        </label>
-      </li>
-      <li className="task-item">
-        <label className="task-item__label">
-          <input type="checkbox" className="task-item__input" />
-          Prepare for the exam
-        </label>
-      </li>
-    </ul>
+    <>
+      <ul className="task-list">
+        {items.map(({ id, title }) => {
+          return (
+            <li key={id} className="task-item">
+              <label className="task-item__label">
+                <input type="checkbox" className="task-item__input" />
+                {title}
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+      {!hasItems && <p className="task-list__placeholder">No Tasks</p>}
+    </>
   );
 };
 
