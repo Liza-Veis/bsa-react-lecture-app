9fc0d1d9cfd37843ef80cd96e6e0f9102a8121e4
Liza-Veis <liza.veis14@gmail.com>
2024-01-24 01:21:06 +0100

    chore: remove combineReducers

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,9 +1,7 @@
-import { combineReducers } from 'redux';
-
 import { reducer as tasksReducer } from './tasks/tasks';
 
-const rootReducer = combineReducers({
+const rootReducer = {
   tasks: tasksReducer,
-});
+};
 
 export { rootReducer };
